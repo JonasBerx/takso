f0d011cbd0e1097dae97baf153d66ddc4c6694f4
Jonas Berx <joberx@ttu.ee>
2021-10-25 17:09:33 +0300

    TDD : added mock taxi drivers for tests

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,8 +1,16 @@
 alias Takso.{Repo, Accounts.User}
+alias Takso.{Repo, Sales.Taxi}
 
 [
   %{name: "Fred Flintstone", username: "fred", password: "parool"},
   %{name: "Barney Rubble", username: "barney", password: "parool"}
 ]
 |> Enum.map(fn user_data -> User.changeset(%User{}, user_data) end)
+|> Enum.each(fn changeset -> Repo.insert!(changeset) end)
+
+[
+  %{username: "juhan", location: "Narva mnt 25", status: "available"},
+  %{username: "armin", location: "Narva mnt 27", status: "available"}
+]
+|> Enum.map(fn taxi_data -> Taxi.changeset(%Taxi{}, taxi_data) end)
 |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
