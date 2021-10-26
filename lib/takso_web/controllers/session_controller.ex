0f5ad030a037df34dec9f094cbb0584742e622b8
Jonas Berx <joberx@ttu.ee>
2021-10-26 10:41:56 +0300

    simple authentication and login system added

---
+++ lib/takso_web/controllers/session_controller.ex
@@ -0,0 +1,27 @@
+defmodule TaksoWeb.SessionController do
+  use TaksoWeb, :controller
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
+    case Takso.Authentication.check_credentials(conn, email, password, repo: Takso.Repo) do
+      {:ok, conn} ->
+        conn
+        |> put_flash(:info, "Welcome #{email}")
+        |> redirect(to: Routes.booking_path(conn, :new))
+
+      {:error, _reason, conn} ->
+        conn
+        |> put_flash(:error, "Bad credentials")
+        |> render("new.html")
+    end
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> Takso.Authentication.logout()
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+end
